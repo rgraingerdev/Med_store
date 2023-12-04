fdaf5d35db69ea02b4b462fa2108cc77a85b50ba
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-04 11:11:19 +0000

    Edit to remove error in settings

--- med_store/wsgi.py
+++ med_store/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for boutique_ado project.
+WSGI config for med_store project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boutique_ado.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'med_store.settings')
 
 application = get_wsgi_application()
 
