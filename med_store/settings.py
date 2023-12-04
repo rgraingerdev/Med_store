6507c18eeb45959891edd033ee674684bd2bfa4c
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-04 13:57:59 +0000

    added profiles for end users

--- med_store/settings.py
+++ med_store/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'product',
     'bag',
+    'profiles',
 
 
     # Other
