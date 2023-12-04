285e9a3842a46078fefc42bd862385e9cfe7754c
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-04 14:14:29 +0000

    Added to settings after server error

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 from django_countries.fields import CountryField
 
-from products.models import Product
+from product.models import Product
 from profiles.models import UserProfile
 
 
